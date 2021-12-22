931ee8bcd7d85324c424b38f37ad740fb1b84042
EmreAlagoz <57771903+EmreAlagoz@users.noreply.github.com>
2021-12-22 22:24:27 +0300

    First commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿
+namespace OSForm2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Thread = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Result = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ElapsedTime = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Thread,
+            this.Result,
+            this.ElapsedTime});
+            this.dataGridView1.Location = new System.Drawing.Point(811, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(595, 797);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.button1.Location = new System.Drawing.Point(223, 546);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(334, 43);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // chart1
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.chart1.Legends.Add(legend3);
+            this.chart1.Location = new System.Drawing.Point(12, 12);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.EarthTones;
+            series3.ChartArea = "ChartArea1";
+            series3.Legend = "Legend1";
+            series3.Name = "Threads";
+            this.chart1.Series.Add(series3);
+            this.chart1.Size = new System.Drawing.Size(793, 481);
+            this.chart1.TabIndex = 3;
+            this.chart1.Text = "chart1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.textBox1.Location = new System.Drawing.Point(223, 511);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(334, 24);
+            this.textBox1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(271, 658);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(212, 18);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "EMRE ALAGÖZ 191180010";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(271, 618);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 18);
+            this.label2.TabIndex = 6;
+            // 
+            // Thread
+            // 
+            this.Thread.HeaderText = "Thread";
+            this.Thread.Name = "Thread";
+            this.Thread.Width = 50;
+            // 
+            // Result
+            // 
+            this.Result.HeaderText = "Result";
+            this.Result.Name = "Result";
+            this.Result.Width = 300;
+            // 
+            // ElapsedTime
+            // 
+            this.ElapsedTime.HeaderText = "Elapsed Time (ms)";
+            this.ElapsedTime.Name = "ElapsedTime";
+            this.ElapsedTime.Width = 200;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1408, 821);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Thread;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Result;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ElapsedTime;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OSForm2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            dataGridView1.RowCount = 32;
+            Control.CheckForIllegalCrossThreadCalls = false;
+        }
+
+        long tenBillion = 10000000000;
+
+        decimal Gauss(decimal num)
+        {
+            return (num * (num + 1)) / 2;
+        }
+
+        decimal Sum(long num)
+        {
+            decimal sum = 0;
+            for (long i = 1; i <= num; i++)
+            {
+                sum += i;
+            }
+
+            return sum;
+        }
+
+        void DoSum(int i)
+        {
+            var watch = Stopwatch.StartNew();
+            decimal sum = Sum(tenBillion);
+            watch.Stop();
+            string message = (i + 1) + ". thread = " + sum + " Elapsed Time = " + watch.ElapsedMilliseconds + "\n";
+            dataGridView1.Rows[i].Cells[0].Value = i + 1;
+            dataGridView1.Rows[i].Cells[1].Value = sum;
+            dataGridView1.Rows[i].Cells[2].Value = watch.ElapsedMilliseconds;
+        }
+
+        public void CreateChart()
+        {
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                if (row!=null)
+                {
+                    chart1.Series["Threads"].Points.AddXY((row.Index) + 1, row.Cells[2].Value);
+                    chart1.ChartAreas[0].AxisX.Title = "Threads";
+                    chart1.ChartAreas[0].AxisY.Title = "Elapsed Time (ms)";
+                }
+                
+            }
+            //chart1.Series["Series1"].Points.AddXY(1, 700);
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            textBox1.Text += ("Gauss Result = " + Gauss(tenBillion) + "\n");
+            var watch_total = Stopwatch.StartNew();
+            Thread[] threads = new Thread[32];
+            for (int i = 0; i < threads.Length; i++)
+            {
+                threads[i] = new Thread(() => DoSum(i));
+                threads[i].Start();
+                threads[i].Join();
+                CreateChart();
+
+            }
+            watch_total.Stop();
+            label2.Text = " Total Elapsed Time = " + watch_total.ElapsedMilliseconds + " ms";
+            //Console.WriteLine(" Total Elapsed Time = " + watch_total.ElapsedMilliseconds + "\n");
+            //CreateChart();
+        }
+    }
+}
